3884260d1fc242d59bdb962318b510260381b2d7
Wayne Werner <wayne@waynewerner.com>
2013-12-10 21:18:32 -0600

    Pivot back to the basics...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
-has_bash=1
-command -v /usr/bin/env bash >/dev/null 2>&1 || { has_bash=0; }
+WGET=/usr/bin/wget
+HAS_WGET=1
+CAN_SUDO=1
 
-if [ $has_bash -eq 1 ]; then
-    echo "Yay we haz a bash - running bash setup!"
-    ./setup.sh
+command -v /usr/bin/wget >/dev/null 2>&1 || { HAS_WGET=0; }
+if [ $HAS_WGET -eq 1 ]; then
+    echo "Yay we haz a wget!"
 else
-    echo "No bash :("
+    echo "No wget :("
+    exit 1
+fi
+
+sudo -v || { CAN_SUDO=0; }
+if [ $CAN_SUDO -eq 1 ]; then
+    echo "Sweet, we have super powers."
+    echo "Grabbing dependencies..."
+    wget -O - https://raw.github.com/waynew/waynes-world/master/aptgettable.sh | sudo sh 
+else
+    echo "Uh oh... not sure if there's anything I can do for you..."
+    echo "I guess more programming is needed"
     exit 1
 fi
 
